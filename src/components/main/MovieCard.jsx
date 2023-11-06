fd410b2f8bffdc9bd86fec69d60de144fb42553a
vkuznetsoff <ss_slavich@mail.ru>
2023-11-06 22:58:41 +0300

    Fix css

--- src/components/main/MovieCard.jsx
+++ src/components/main/MovieCard.jsx
@@ -3,13 +3,13 @@ const MovieCard = ({card}) => {
   debugger
   return (
     <div className="row">
-      
       <div class="card">
         <div class="card-image">
           <img src={Poster} />
           <span class="card-title">{Title}</span>
-          <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+          {/* <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a> */}
         </div>
+        
         <div class="card-content">
           <p>Description</p>
         </div>
