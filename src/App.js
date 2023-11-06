8e4e36c0c1789afd135f479bc1f5f1efe393e4c2
vkuznetsoff <ss_slavich@mail.ru>
2023-11-06 19:11:46 +0300

    Add footer header, basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <div className="App">
-      HELLO
+     <Header />
+     <Main />
+     <Footer />
+
     </div>
   );
 }
